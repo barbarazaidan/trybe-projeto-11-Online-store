da3b8512d0af7519ecfbccb9de4eab8722dbad4f
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-01-12 16:36:12 -0300

    [6] = Requisito

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -55,7 +55,7 @@ class Main extends Component {
 
   render() {
     const { categories, returnSearchProducts } = this.state;
-    // console.log(returnSearchProducts);
+    // console.log(returnSearchProduc);
 
     return (
       <>
