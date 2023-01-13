49a7df43fc5321e51f3d2488d73bc739422f2f86
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2023-01-12 21:14:32 -0300

    [8] = Requisito

--- src/pages/Carrinho.js
+++ src/pages/Carrinho.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
 class Carrinho extends React.Component {
+  state = {
+    produtos: [],
+  };
+
+  componentDidMount() {
+    const produtosSalvos = JSON.parse(localStorage.getItem('Produtos'));
+    this.setState({ produtos: produtosSalvos });
+  }
+
   render() {
+    const { produtos } = this.state;
     return (
-      <div data-testid="shopping-cart-empty-message">Seu carrinho está vazio</div>
+      <div data-testid="shopping-cart-empty-message">
+        <p data-testid="shopping-cart-product-quantity">
+          {produtos.length || 'Seu carrinho está vazio'}
+        </p>
+        <div>
+          <div>
+            {produtos.map((produto) => (
+              <div key={ produto.id }>
+                <h2 data-testid="shopping-cart-product-name">
+                  {produto.title}
+                </h2>
+                <p>
+                  Preço:
+                  {
+                    produto.price
+                  }
+                </p>
+                <p>{produto.quantity}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     );
   }
 }
